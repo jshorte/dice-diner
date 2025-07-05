82769e94799ad21d1e3d71a74a4c2f551a1b8252
Jacob Shorte <jacob.shorte@gmail.com>
2025-07-05 19:28:17 +0100

    Folder structure

---
+++ Scripts/Dice/dice_resource_template.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name Dice_template
+
+var definitions = load("res://Scripts/g_enum.gd")
+
+@export var dice_min : int
+@export var dice_max : int
+@export var dice_interval : int 
+@export var dice_sprite_animation_path : String
+@export var dice_type: G_ENUM.DiceType
+@export var dice_name : String
---
+++ Scripts/g_enum.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name G_ENUM
+
+enum DiceType { PIZZA, GARLIC }
+enum DiceState { DISABLED, ACTIVE, PASSIVE }
+enum GameState { SELECT, PLAY, SCORE }
